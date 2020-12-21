6d4b9414ec0e00dbf8e76e4a1a5a3850540b5ceb
Zach Pate <>
2020-12-20 19:15:01 -0500

    still flushing out add vehicle page

--- MaintenanceTracker/AddVehicleView.swift
+++ MaintenanceTracker/AddVehicleView.swift
@@ -11,23 +11,49 @@ import SwiftUI
 struct AddVehicleView: View {
 	
 	@Binding var showAddVehiclePopover: Bool
+	@State var name: String = ""
 	@State var make: String = ""
 	@State var model: String = ""
 	@State var year: String = "2010"
 	
 	var body: some View {
-		NavigationView {
-            Form {
-				Picker(selection: $year, label: Text("Vehicle Year")) {
-					ForEach(1900 ..< 2022) {
-						Text(String($0))
+		VStack {
+			NavigationView {
+				Form {
+					// TODO: Figure out why this picker is throwing a constraint error
+					Picker(selection: $year, label: Text("Vehicle Year")) {
+						ForEach(1900 ..< 2022) {
+							Text(String($0))
+						}
 					}
+					TextField("Vehicle Make", text: $make)
+					TextField("Vehicle Model", text: $model)
 				}
-				TextField("Vehicle Make", text: $make)
-				TextField("Vehicle Model", text: $model)
+				.navigationBarTitle("Add New Vehicle")
 			}
-            .navigationBarTitle("Add New Vehicle")
-        }
+			Spacer()
+			Button(action: {
+				self.createVehicle()
+			}) {
+				Text("Add New Vehicle")
+				.fontWeight(.semibold)
+				.font(.title)
+				.frame(minWidth: 0, maxWidth: .infinity)
+				.padding()
+				.foregroundColor(.white)
+				.background(Color.blue)
+				.cornerRadius(40)
+			}
+			// TODO: Create disabled style for button
+//			.disabled(make.isEmpty || name.isEmpty || model.isEmpty || year.isEmpty)
+		}
+	}
+	
+	
+	func createVehicle() {
+		let myVehicle = Vehicle(name: name, make: make, model: model, year: year)
+		
+		print("CREATED VEHICLE \(myVehicle)")
 	}
 }
 
--- MaintenanceTracker/Model/Vehicle.swift
+++ MaintenanceTracker/Model/Vehicle.swift
@@ -12,9 +12,9 @@ class Vehicle {
 	var name: String
 	var make: String
 	var model: String
-	var year: Int
+	var year: String
 	
-	init(name: String, make: String, model: String, year: Int) {
+	init(name: String, make: String, model: String, year: String) {
 		self.name = name
 		self.make = make
 		self.model = model
